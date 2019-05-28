f097e13e777f7892e71a76271fd550b5a36bd29c
Henry Zimmerman <zimhen7@gmail.com>
2019-05-28 07:14:40 -0400

    fixed build scripts

--- docker/build_dev.sh
+++ docker/build_dev.sh
@@ -3,4 +3,4 @@
 cd ../frontend;
 npm install && npm run-script build
 cd ../backend
-bash ../docker/wait_for_it.sh db:5432 -q -- diesel setup --migration-dir db/migrations && cargo run --bin server_bin --release -- --server-lib-root ./server --port 8080 --staging
+bash ../docker/wait_for_it.sh db:5432 -q -- diesel setup --migration-dir migrations && cargo run --bin server_bin --release -- --server-lib-root ./server --port 8080 --staging
